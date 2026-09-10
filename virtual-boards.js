b5538f63c900b9d0593fb49fe20c21c8239c66b9
nikolay5555 <ngbonev1@gmail.com>
2026-09-10 10:08:54 +0300

    Update virtual-boards.js

--- virtual-boards.js
+++ virtual-boards.js
@@ -1053,7 +1053,7 @@
         selectedStopMarker.setStyle({
           fillColor: "#111827",
           color: "#ffffff",
-          fillOpacity: 0.9
+          fillOpacity: 1
         });
         selectedStopMarker = null;
       }
@@ -1144,7 +1144,7 @@
       selectedStopMarker.setStyle({
         fillColor: "#111827",
         color: "#ffffff",
-        fillOpacity: 0.9
+        fillOpacity: 1
       });
     }
     const lat = Number(stop.stop_lat);
@@ -1314,7 +1314,7 @@
         weight: 2,
         color: "#ffffff",
         fillColor: "#111827",
-        fillOpacity: 0.9,
+        fillOpacity: 1,
         renderer,
         pane: "markerPane"
       });
